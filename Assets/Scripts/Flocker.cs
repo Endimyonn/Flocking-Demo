21992ae21f9de3db7c078078d25f98162d9521ab
Endimyonn <joeakuaku@gmail.com>
2022-04-21 14:01:24 -0400

    begin writing crow/flocker logic

--- Assets/Scripts/Flocker.cs
+++ Assets/Scripts/Flocker.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Flocker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("General")]
+    public int numCrows = 25;
+    private int lastCrows;
+    public GameObject crowPrefab;
+    [HideInInspector] public List<Crow> crows;
+
+    [Header("Flock parameters")]
+    public float flockCohesionLimit = 13f;
+    public float crowSeparationDist = 2.1f;
+    public float crowFOV = 270f;
+
+    [Header("Spawning area")]
+    public float spawnAreaX;
+    public float spawnAreaY;
+    public float spawnHeight = 3f;
+
+    private void Awake()
     {
-        
+        lastCrows = numCrows;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SpawnCrows()
     {
-        
+        for (int i = 0; i < numCrows; i++)
+        {
+            SpawnCrow();
+        }
+    }
+
+    public void SpawnCrow()
+    {
+        Vector3 crowPosition = new Vector3(Random.Range(-spawnAreaX, spawnAreaX), 0, Random.Range(-spawnAreaY, spawnAreaY));
+        GameObject crow = GameObject.Instantiate(crowPrefab, crowPosition, Quaternion.Euler(crowPrefab.transform.eulerAngles.x, 0, Random.Range(0, 360));
+        crow.GetComponent<Crow>().flocker = this;
+        //init crow?
     }
 }
